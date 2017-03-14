b3d1d77774029b0deb6616186f547e3af19f10b5
eracah <ejracah@gmail.com>
2017-03-14 14:39:08 -0700

    recent changes to network

--- 2d_semi_sup/notebooks/make_network.py
+++ 2d_semi_sup/notebooks/make_network.py
@@ -30,15 +30,15 @@ from os.path import join
 
 # In[2]:
 
-#%matplotlib inline
+
 
 
 # In[3]:
 
-cl_args = {"lr": 0.01,
+cl_args = {"lr": 0.00001,
            "num_epochs": 20, 
            "filters_scale": 1./8,
-           "data_dir": "smallish_dataset", 
+           "data_dir": "extremely_small_dataset", 
            "save_dir": "/global/homes/r/racah/projects/climate-caffe/2d_semi_sup/notebooks/plots" }
 
 
@@ -62,9 +62,6 @@ basepath = "/global/homes/r/racah/projects/climate-caffe/2d_semi_sup/notebooks/p
 tr_batch_size=4
 val_batch_size=16
 
-
-# In[6]:
-
 tr_pstr, tr_netspec, num_tr = make_netcdf_network(batch_size=tr_batch_size,
                                                   prefix_dir=cl_args["data_dir"],
                                                   mode="tr", 
@@ -78,7 +75,7 @@ val_pstr, val_netspec, num_val = make_netcdf_network(batch_size=val_batch_size,p
 _, val_net_filepath = write_to_file(val_netspec, filename="val", basepath=basepath, )
 
 
-# In[7]:
+# In[6]:
 
 spstr,solver_filename = make_solver(net_path=basepath,base_lr=cl_args["lr"],
                                     train_file_name="train.prototxt", 
@@ -87,77 +84,91 @@ spstr,solver_filename = make_solver(net_path=basepath,base_lr=cl_args["lr"],
                                     test_batch_size=val_batch_size, print_every_iteration=10000)
 
 
-# In[8]:
-
-
-
-
-# In[9]:
+# In[7]:
 
 solver = caffe.SGDSolver(solver_filename)
 
 
 # In[ ]:
 
-tr_its_per_epoch = int(float(num_tr) / tr_batch_size)
-val_its_per_epoch = int(float(num_val) / val_batch_size)
-tr_ep_losses = []
-test_ep_losses = []
-it_losses = []
-# cls_losses = []
-# rec_losses = []
-num_epochs = cl_args["num_epochs"]
-for ep in range(num_epochs):
-    for _ in range(tr_its_per_epoch):
-        solver.step(1)
-
-        loss = np.float32(solver.net.blobs['final_loss'].data)
-        #rec_losses.append(np.float32(solver.net.blobs['L_rec'].data))
-        #cls_losses.append(np.float32(solver.net.blobs['L_cls'].data))
-        it_losses.append(loss)
-    
-
-    #sys.stderr.write("TR CLS LOSS: " + str(np.mean(cls_losses)))
-    #print "TR REC LOSS: ", np.mean(rec_losses)
-    ep_loss = np.mean(it_losses)
-    
-    sys.stderr.write("\nEPOCH TR: " + str(ep_loss))
-    tr_ep_losses.append(ep_loss)
-    it_losses = []
-#     cls_losses = []
-#     rec_losses = []
-    
-    
-#     for _ in range(val_its_per_epoch):
-#         tn=solver.test_nets[0]
-#         loss = np.float32(tn.forward()["final_loss"].data)
-#         it_losses.append(loss)
-    
-#     test_ep_loss = np.mean(it_losses)
-#     test_ep_losses.append(test_ep_loss)
-#     sys.stderr.write("\nEPOCH VAL: " + str(test_ep_loss))
-#     it_losses = []
-    
-    
-    suffix = "learning_curve_lr_%4.2f_fs_%4.3f_%s.jpg"%(cl_args["lr"], cl_args["filters_scale"], cl_args["data_dir"])
-    plt.figure(1)
-    plt.plot(tr_ep_losses)
-    plt.savefig(join(cl_args["save_dir"],"tr_" + suffix))
-#     plt.figure(2)
-#     plt.plot(test_ep_losses)
-#     plt.savefig(join(cl_args["save_dir"],"val_" + suffix))
-   
-    
+#%matplotlib inline
 
 
 # In[8]:
 
+tr_its_per_epoch = int(float(num_tr) / tr_batch_size)
+val_its_per_epoch = int(float(num_val) / val_batch_size)
+num_epochs = cl_args["num_epochs"]
+
+loss_keys = ["L_cls",
+"L_obj",
+"L_xy", 
+"L_wh",
+"L_rec",
+"final_loss"]
+losses = {}
+for ep in range(num_epochs):
+    losses[ep] = {k:[] for k in loss_keys}
+    losses[str(ep) + "_mean"] = {k:0 for k in loss_keys}
+    for it in range(tr_its_per_epoch):
+        solver.step(1)
+
+        for k in loss_keys:
+            loss = np.float32(solver.net.blobs[k].data)
+            losses[ep][k].append(loss)
+            
+
+
+    losses[str(ep) + "_mean"] = {k:np.mean(losses[ep][k]) for k in loss_keys}
+    
+    
+    for k in loss_keys:
+        loss = losses[str(ep) + "_mean"][k]
+        sys.stderr.write("\n Epoch %i: Final %s Loss = %6.3f" % (ep, k, loss))
+        
+        
+        
+
+    
+    
+    suffix = "learning_curve_lr_%8.6f_fs_%4.3f_%s.jpg"%(cl_args["lr"], cl_args["filters_scale"], cl_args["data_dir"])
+    plt.figure(1)
+    plt.plot([losses[str(epoch) + "_mean"]["final_loss"] for epoch in range(ep + 1)])
+    #plt.show()
+    plt.savefig(join(cl_args["save_dir"],"tr_" + suffix))
+
+   
+    
+
+
+# In[ ]:
+
 #! jupyter nbconvert --to script make_network.ipynb
 
 
-# In[15]:
+# In[ ]:
+
+
+
+
+# In[11]:
 
 # gb_basedir = "/project/projectdirs/dasrepo/gordon_bell/deep_learning/networks/climate/2d_semi_sup"
+
+# tr_batch_size=4
+# val_batch_size=16
+
+# tr_pstr, tr_netspec, num_tr = make_netcdf_network(batch_size=tr_batch_size,
+#                                                   prefix_dir=cl_args["data_dir"],
+#                                                   mode="tr", 
+#                                                   filters_scale=cl_args["filters_scale"] )
+
+
+
+# _, tr_net_filepath = write_to_file(tr_netspec, filename="train", basepath=gb_basedir)
+
+# val_pstr, val_netspec, num_val = make_netcdf_network(batch_size=val_batch_size,prefix_dir=cl_args["data_dir"], mode="val" ,filters_scale=cl_args["filters_scale"])
+# _, val_net_filepath = write_to_file(val_netspec, filename="val", basepath=gb_basedir, )
 
 # val_net_filepath = join(gb_basedir, "val.prototxt")
 
@@ -166,6 +177,10 @@ for ep in range(num_epochs):
 #                                     test_net_path=val_net_filepath, tr_num_examples=num_tr, 
 #                                     test_num_examples=num_val,tr_batch_size=tr_batch_size, 
 #                                     test_batch_size=val_batch_size, print_every_iteration=10000)
+
+# solver = caffe.SGDSolver(solver_filename)
+
+# solver.step(1)
 
 
 # In[ ]:
